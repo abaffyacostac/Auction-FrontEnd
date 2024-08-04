b023623f84759e234a35696105c486915f71576a
abaffyacostac <abaffyacostac@student.douglascollege.ca>
2024-08-04 00:35:46 -0400

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header-logo">
-        <img src="./src/assets/Eauction.png" alt="E-Auction Logo" />
+        <img src="./assets/Eauction.png" alt="E-Auction Logo" />
       </div>
       <h1 className="header-title">Electronic - Auction</h1>
     </header>
